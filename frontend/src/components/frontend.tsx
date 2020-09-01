f7c526e1928e8fda02453f0c2e0e758a906108c3
Manuel Vila <mvila@3base.com>
2020-09-01 12:30:55 +0900

    frontend: Move Root.Main view into Frontend component

--- frontend/src/components/frontend.tsx
+++ frontend/src/components/frontend.tsx
@@ -1,9 +1,10 @@
 import {Component, provide} from '@liaison/component';
 import {Storable} from '@liaison/storable';
 import {ComponentHTTPClient} from '@liaison/component-http-client';
+import React from 'react';
+import {view, useBrowserRouter} from '@liaison/react-integration';
 
 import type {Movie as BackendMovie} from '../../../backend/src/components/movie';
-import {Root} from './root';
 import {MovieList} from './movie-list';
 import {Movie} from './movie';
 import {Common} from './common';
@@ -14,10 +15,26 @@ export const getFrontend = async ({backendURL}: {backendURL: string}) => {
   const BackendMovieProxy = (await client.getComponent()) as typeof BackendMovie;
 
   class Frontend extends Component {
-    @provide() static Root = Root;
     @provide() static MovieList = MovieList;
     @provide() static Movie = Movie(BackendMovieProxy);
     @provide() static Common = Common;
+
+    @view() static Main() {
+      const [router, isReady] = useBrowserRouter(this);
+
+      if (!isReady) {
+        return null;
+      }
+
+      const content = router.callCurrentRoute({fallback: this.Common.RouteNotFound});
+
+      return (
+        <div>
+          <h1>CRUD example app</h1>
+          {content}
+        </div>
+      );
+    }
   }
 
   return Frontend;
